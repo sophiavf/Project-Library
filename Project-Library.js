e418c73a94f4e6ea9185e8921dd674e2ce8f37bc
sophiavf <15851587+sophiavf@users.noreply.github.com>
2023-02-06 23:25:11 +1300

    Created function to add books to array and add element to the webpage

--- Project-Library.js
+++ Project-Library.js
@@ -1,30 +1,63 @@
 let myLibrary = [];
 
+// the constructor
 function Book(title, author, pages, read) {
-	// the constructor...
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
 	this.read = read;
 	this.reportInfo = function () {
-		return `${title} by $author}, ${pages}, ${read} read yet`;
+		return `${title} by ${author}, ${pages}, ${read} read yet`;
 	};
 }
 
-function addBookToLibrary(book) {
-	//'NEW BOOK' button brings up a form allowing users to input the details for the new book: author, title, number of pages,whether it’s been read and anything else you might want.
-	//take user’s input
-	// store the new book objects into an array
+function addBookToLibrary(title, author, pages, read) {
+	//4. store the new book objects into an array
+	let book = new Book(title, author, pages, read);
 	this.book = book;
 	myLibrary.push(book);
 }
 
+const libraryDisplay = document.querySelector(".bookDisplay");
+
 function displayBooks() {
 	//Loops through books
+	myLibrary.forEach((book) => {
+		let newBookElement = document.createElement("div");
+		newBookElement.innerHTML = `<p>${book.reportInfo()}</p>
+		<button class='removeBookButton' data-key='${book.title}'>Remove</button>
+		<button class='changeReadStatus' data-key='${book.title}'>${
+			book.read
+		} read</button>`;
+		newBook.setAttribute("data-key", title);
+		libraryDisplay.appendChild(newBookElement);
+	});
 	//Displays each book on the page
 }
 
-const newBookButton = document.getElementsByClassName("newBook");
-const removeBookButton = document.createElement("button.removeBook");
+//1. 'NEW BOOK' button brings up a form allowing users to input the details for the new book: author, title, number of pages,whether it’s been read and anything else you might want.
+const newBookButton = document.querySelector("#newBook");
+newBookButton.addEventListener("click", function () {
+	document.getElementById("popupForm").style.display = "block";
+});
 
-newBookButton.addEventListener;
+//2. Brings up form
+//3. takes user’s input
+
+//Removing a book
+var removeBookButtons = document.querySelector(".removeBookButton");
+if (removeBookButtons !== null) {
+	removeBookButtons.forEach((button) => {
+		button.addEventListener("click", () => {
+			const index = myLibrary.indexOf(button.data("value"));
+			myLibrary = myLibrary.splice(index, 1);
+		});
+	});
+}
+
+//Changing
+// if (removeBookButtons.length > 0)
+
+//testing
+addBookToLibrary("Bible", "Joe", 267, "not");
+displayBooks();
