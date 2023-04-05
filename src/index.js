aca90cb8ea955e186913cff759458c1c84186a91
sophia <15851587+sophiavf@users.noreply.github.com>
2023-04-05 18:17:16 +0200

    - Removed test cases from index.js - Re-ran build command

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import "./styling.css";
 import DomManipulation from "./modules/DomManipulation";
 import Library from "./modules/Library";
 
-
-Library.addBookToLibrary("Big", "New", 12, false);
-Library.addBookToLibrary("Small", "New", 12, true);
-Library.addBookToLibrary("Medium", "New", 12, false);
-
 DomManipulation.initializeListeners();
 DomManipulation.loadFooter();
 DomManipulation.displayBooks(); 
